cfd03d03bcefcdd9ce954335a96b81c9106ba509
CirnoSamaS <32702915+CirnoSamaS@users.noreply.github.com>
2020-12-06 00:37:12 +0800

    啊
    update

---
+++ js/web.js
@@ -0,0 +1,37 @@
+
+//获取当前时间
+var date=new Date();
+var year=date.getFullYear();
+//月
+var month=(date.getMonth()+1)<10?"0"+(date.getMonth()+1):(date.getMonth()+1);
+//日
+var day=date.getDate()<10?"0"+date.getDate():date.getDate();
+//时
+var hh=date.getHours()<10?"0"+date.getHours():date.getHours();
+//分
+var mm=date.getMinutes()<10?"0"+date.getMinutes():date.getMinutes();
+//秒
+var ss=date.getSeconds()<10?"0"+date.getSeconds():date.getSeconds();
+
+var nowTime = year+"-"+month+"-"+day+" "+hh+":"+mm+":"+ss;
+
+document.getElementById("time1").innerHTML="核验时间： "+nowTime;
+var Outtime=180;
+setInterval(
+function(){
+	if(Outtime!=0)
+	Outtime=Outtime-1;
+	else
+	Outtime=0;
+	document.getElementById("time2").innerHTML=Outtime;
+},1000);
+//随机获得学号
+var id=["201835030311","201723130318","201823130818","201998030101","202023130448"]
+var idname=["王华","吴亦凡","张东申","陈冠南","朱晓川"];
+var img_id=['1','2','3','4','5'];
+var randomid = id[Math.floor(Math.random() * id.length)];
+var randomname = idname[Math.floor(Math.random() * idname.length)];
+var randomimg= img_id[Math.floor(Math.random() * img_id.length)];
+document.getElementById("xuehao1").innerHTML=randomname+'-'+randomid;;
+document.getElementById("xuehao2").innerHTML=randomid;
+document.getElementById("img_id").src="./img/"+randomimg+".jpg";
